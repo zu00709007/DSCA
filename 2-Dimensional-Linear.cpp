7dc5539f2a593b5660fe915eb46f74cdf9ee0ff6
JiYen <s0917170207@gmail.com>
2017-11-21 00:06:01 +0800

    modified:   2-Dimensional-Linear.cpp

--- 2-Dimensional-Linear.cpp
+++ 2-Dimensional-Linear.cpp
@@ -76,11 +76,10 @@ int main()
         scanf("%d", &data[i].c);
         data[i].next = -1;
     }
-	
 
     // split the data into positive and negative
-	float xl = -INFINITY, xr = INFINITY;
-	tmp = 0;
+    float xl = -INFINITY, xr = INFINITY;
+    tmp = 0;
     for(i=0; i<line; ++i)
     {
         if(0 > data[i].y)
@@ -99,17 +98,17 @@ int main()
                 data[j].next = i;
             j = i;
         }
-		else
-		{
-			if(0 < data[i].x && data[i].x < xr)
-				xr = (float)data[i].c / data[i].x;
-			else if(data[i].x > xl)
-				xl = (float)data[i].c / data[i].x;
-			++tmp;
-		}
+        else
+        {
+            if(0 < data[i].x && data[i].x < xr)
+                xr = (float)data[i].c / data[i].x;
+            else if(data[i].x > xl)
+                xl = (float)data[i].c / data[i].x;
+            ++tmp;
+        }
     }
-	line -= (int)tmp;
-    // start Megiddo Algorithm, prune the line into 3    
+    line -= (int)tmp;
+    // start Megiddo Algorithm, prune the line into 3
     while(3 < line)
     {
         // remove the unnecessary line
@@ -203,9 +202,8 @@ int main()
             tmp -= 0.5;
         if(tmp == xl)
             tmp += 0.5;
-		
-		if(xr - xl == 0.5)
-			break;
+        if(xr - xl == 0.5)
+            break;
 
         // find the intersection in median
         float alpha = -INFINITY, belta = INFINITY, alpha_m1, alpha_m2, belta_m1, belta_m2, tmp2;
@@ -254,10 +252,10 @@ int main()
                     xl = tmp;
             }
             else
-			{
-				printf("-INF\n");
-				return 0;
-			}                
+            {
+                printf("-INF\n");
+                return 0;
+            }
         }
         else
         {
@@ -266,18 +264,17 @@ int main()
             else if((alpha_m1 < alpha_m2 ? alpha_m1 : alpha_m2) > (belta_m1 > belta_m2 ? belta_m1 : belta_m2))
                 xr = tmp;
             else
-			{
-				printf("NA\n");
-				return 0;
-			}                
+            {
+                printf("NA\n");
+                return 0;
+            }
         }
     }
     // concat all line in neg array
     i = neg;
     while(-1 < i && -1 < data[i].next)
         i = data[i].next;
-    data[i].next = pos;		
-	
+    data[i].next = pos;
 
     // find all intersection and x=0
     float inter_x[100], inter_y[100], min_y = INFINITY, tmp2;
@@ -305,23 +302,23 @@ int main()
         inter_x[j] = tmp;
         inter_y[j] = tmp2;
     }
-	
-	// find the intersection of xl and xr
-	i = neg;
+
+    // find the intersection of xl and xr
+    i = neg;
     while(-1 < i)
     {
-		if(xl != -INFINITY)
-		{
-			++j;
-			inter_x[j] = xl;
-			inter_y[j] = ((float)data[i].c - data[i].x * xl) / data[i].y;
-		}
-		if(xr != INFINITY)
-		{
-			++j;
-			inter_x[j] = xr;
-			inter_y[j] = ((float)data[i].c - data[i].x * xr) / data[i].y;
-		}		
+        if(xl != -INFINITY)
+        {
+            ++j;
+            inter_x[j] = xl;
+            inter_y[j] = ((float)data[i].c - data[i].x * xl) / data[i].y;
+        }
+        if(xr != INFINITY)
+        {
+            ++j;
+            inter_x[j] = xr;
+            inter_y[j] = ((float)data[i].c - data[i].x * xr) / data[i].y;
+        }
         i = data[i].next;
     }
 
@@ -342,33 +339,33 @@ out:
         ++i;
     }
     if(INFINITY == min_y)
-		printf("NA\n");  
+        printf("NA\n");
     else
-	{
-		i = neg;
-		while(-1 < i)
-		{
-			if(data[i].x == 0)
-				goto out2;
-			inter_y[0] = min_y - 10;
-			inter_x[0] = ((float)data[i].c - data[i].y*inter_y[0]) / data[i].x;
-			k = neg;
-			while(-1 < k)
-			{
-				if(QuickFist(inter_x[0] * data[k].x + inter_y[0] * data[k].y) > data[k].c)
-					goto out2;
-				k = data[k].next;
-			}
-			if(inter_x[0] >= xl && inter_x[0] <= xr)
-			{
-				printf("-INF\n");
-				return 0;
-			}
+    {
+        i = neg;
+        while(-1 < i)
+        {
+            if(data[i].x == 0)
+                goto out2;
+            inter_y[0] = min_y - 10;
+            inter_x[0] = ((float)data[i].c - data[i].y*inter_y[0]) / data[i].x;
+            k = neg;
+            while(-1 < k)
+            {
+                if(QuickFist(inter_x[0] * data[k].x + inter_y[0] * data[k].y) > data[k].c)
+                    goto out2;
+                k = data[k].next;
+            }
+            if(inter_x[0] >= xl && inter_x[0] <= xr)
+            {
+                printf("-INF\n");
+                return 0;
+            }
 out2:
-			i = data[i].next;
-		}
-		printf("%d\n", QuickFist(min_y));
-	}
-    
+            i = data[i].next;
+        }
+        printf("%d\n", QuickFist(min_y));
+    }
+
     return 0;
 }
