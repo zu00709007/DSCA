4f1afe38293450a53aee87e67297131ff538956f
jiyen <s0917170207@gmail.com>
2018-01-06 00:39:08 +0800

    Add files via upload

---
+++ 01_Knapsack_Branch&Bound.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<algorithm>
+
+using namespace std;
+int w, n, lb = 0;
+
+struct Item
+{
+    int value, weight;
+};
+struct Item item[1000];
+
+int bound(int value, int weight, int level)
+{
+    if (weight >= w)
+        return 0;
+
+    while(level < n && weight + item[level].weight <= w)
+    {
+        weight += item[level].weight;
+        value += item[level].value;
+        ++level;
+    }
+    if(level < n)
+        value += (w - weight) * item[level].value / item[level].weight;
+    return value;
+}
+
+void knapsack(int value, int weight, int level)
+{
+    if(level == n || lb >= bound(value, weight, level))
+        return;
+
+    int new_weight = weight + item[level].weight, new_value = value + item[level].value;
+    if(new_weight <= w && new_value > lb)
+        lb = new_value;
+
+    knapsack(new_value, new_weight, level + 1);
+    knapsack(value, weight, level + 1);
+}
+
+bool for_sort(struct Item a, struct Item b)
+{
+    return (double)a.value / a.weight > (double)b.value / b.weight;
+}
+
+int main()
+{
+    scanf("%d %d", &w, &n);
+    for(int i=0; i<n; ++i)
+        scanf("%d %d", &item[i].value, &item[i].weight);
+
+    sort(item, item + n, for_sort);
+    knapsack(0, 0, 0);
+    printf("%d\n", lb);
+    return 0;
+}
